d2430f94e84110f8275ff325246841d1b9d354d8
Michał <michal@pop-os>
2020-10-30 10:36:16 +0100

    Update GUI.py and functions.py

--- functions.py
+++ functions.py
@@ -8,6 +8,19 @@ def ins_to_txtb(number_or_sign):
     GUI.txtBox.config(state=DISABLED)
 
 
-def push_operation(sign):
-    GUI.txtBox.get('1.0', 'end-1c')
+def equal():
+    try:
+        equation = GUI.txtBox.get('1.0', 'end-1c')
+        result = eval(equation)
+        GUI.txtBox.config(state=NORMAL)
+        GUI.txtBox.insert(END, "\n" + str(result))
+        GUI.txtBox.config(state=DISABLED)
+    except:
+        print('error!')
+
+
+def clean():
+    GUI.txtBox.config(state=NORMAL)
+    GUI.txtBox.delete('1.0', END)
+    GUI.txtBox.config(state=DISABLED)
 
