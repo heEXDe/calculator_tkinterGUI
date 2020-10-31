e26f9d3547f71020d88f0b65e7224b38d21af472
Michał <michal@pop-os>
2020-10-31 14:38:54 +0100

    Update GUI.py and functions.py

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@ from tkinter import *
 import GUI
 
 
-def ins_to_txtb(number_or_sign):
+def inp_to_txtb(number_or_sign):
     GUI.txtBox.config(state=NORMAL)
     GUI.txtBox.insert(END, str(number_or_sign))
     GUI.txtBox.config(state=DISABLED)
