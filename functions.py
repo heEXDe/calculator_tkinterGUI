763ae032d65c4ab0f0e091a8f1e2307a1b64c430
heEXDe <61631314+heEXDe@users.noreply.github.com>
2020-11-04 21:08:38 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,44 +1,59 @@
-from tkinter import *
+# functions for actions
+import random
+import string
 import GUI
-from math import log as log
-from math import sqrt as sqrt
-from math import sin as sin
-from math import cos as cos
 
 
-def inp_to_txtb(number_or_sign):
-    GUI.txtBox.config(state=NORMAL)
-    GUI.txtBox.insert(END, str(number_or_sign))
-    GUI.txtBox.config(state=DISABLED)
+def generate_password():
+    password = ''
+    if (GUI.varDigi.get() == 1) & (GUI.varChLower.get() == 1) & (GUI.varChUpper.get() == 1):
+        strin = string.ascii_letters
+        for i in range(10):
+            chornumb = random.choice(['ch', 'digi'])
+            if chornumb == 'ch':
+                password = password + random.choice(strin)
+            else:
+                password = password + str(random.randint(0, 10))
+    elif (GUI.varDigi.get() == 1) & (GUI.varChLower.get() == 1) & (GUI.varChUpper.get() == 0):
+        strin = string.ascii_lowercase
+        for i in range(10):
+            chornumb = random.choice(['ch', 'digi'])
+            if chornumb == 'ch':
+                password = password + random.choice(strin)
+            else:
+                password = password + str(random.randint(0, 10))
+    elif (GUI.varDigi.get() == 1) & (GUI.varChLower.get() == 0) & (GUI.varChUpper.get() == 1):
+        strin = string.ascii_uppercase
+        for i in range(10):
+            chornumb = random.choice(['ch', 'digi'])
+            if chornumb == 'ch':
+                password = password + random.choice(strin)
+            else:
+                password = password + str(random.randint(0, 10))
+    elif (GUI.varDigi.get() == 0) & (GUI.varChLower.get() == 1) & (GUI.varChUpper.get() == 1):
+        strin = string.ascii_letters
+        for i in range(10):
+            password = password + random.choice(strin)
+    elif (GUI.varDigi.get() == 0) & (GUI.varChLower.get() == 0) & (GUI.varChUpper.get() == 1):
+        strin = string.ascii_uppercase
+        for i in range(10):
+            password = password + random.choice(strin)
+    elif (GUI.varDigi.get() == 0) & (GUI.varChLower.get() == 1) & (GUI.varChUpper.get() == 0):
+        strin = string.ascii_lowercase
+        for i in range(10):
+            password = password + random.choice(strin)
+    elif (GUI.varDigi.get() == 1) & (GUI.varChLower.get() == 0) & (GUI.varChUpper.get() == 0):
+        for i in range(10):
+            password = password + str(random.randint(0, 10))
+    else:
+        GUI.lblError.config(text='error!')
+    # print(password)
+    print(str(GUI.varDigi.get()) + ', ' + str(GUI.varChLower.get()) + ', ' + str(GUI.varChUpper.get()))
+    # print(strin)
+    GUI.lblPassword.config(text=password)
 
 
-def equal():
-    try:
-        equation = GUI.txtBox.get('1.0', 'end-1c')
-        result = eval(equation)
-        GUI.txtBox.config(state=NORMAL)
-        GUI.txtBox.insert(END, "\n" + '= ' + str(result))
-        GUI.txtBox.config(state=DISABLED)
-    except:
-        GUI.txtBox.config(state=NORMAL)
-        GUI.txtBox.insert(END, "\n" + "error")
-        GUI.txtBox.config(state=DISABLED)
-
-
-def clean():
-    GUI.txtBox.config(state=NORMAL)
-    GUI.txtBox.delete('1.0', END)
-    GUI.txtBox.config(state=DISABLED)
-
-
-def backspace():
-    try:
-        s_str = GUI.txtBox.get('1.0', 'end-1c')
-        res_str = ''.join([s_str[i] for i in range(len(s_str)) if i != len(s_str) - 1])
-        GUI.txtBox.config(state=NORMAL)
-        GUI.txtBox.delete('1.0', END)
-        GUI.txtBox.insert(END, str(res_str))
-        GUI.txtBox.config(state=DISABLED)
-    except:
-        print('error!')
-
+def copy_pass():
+    toclpboard = GUI.lblPassword.cget("text")
+    GUI.root.clipboard_clear()
+    GUI.root.clipboard_append(toclpboard)
